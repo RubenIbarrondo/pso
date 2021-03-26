486ff5b8e19b6e37d77921697f378bb744571863
RubenIbarrondo <rubenibarrondo@gmail.com>
2021-03-26 11:50:51 +0100

    Update rr-pso.py.

--- rr_pso.py
+++ rr_pso.py
@@ -28,3 +28,32 @@ def update_xv(x, v, dt, a1, a2, gbest, lbest, omega):
     return x_new, v_new
 
 
+def rr_pso(x0, v0, dt, a1, a2, omega, criteria, memory=False, max_iter=int(1e6)):
+    """
+
+    :param x0: vector de valores inciales, (numero_de_partículas x numero_de_parametros)
+    :param v0: vector de velocidades iniciales, (numero_de_partículas x numero_de_parametros)
+    :param dt: salto de tiempo, float
+    :param a1: aceleración al grobal best, float
+    :param a2: aceleración al local best, float :param dt:
+    :param omega: inercia, float
+    :param criteria: función objetivo, function
+        criteria(x) -> fx : vector de valores de la función objetivo para cada partícula (numero_de_partículas)
+    :param memory: si quieres guardar memoria, boolean
+    :param max_iter: número máximo de interaciones, int
+    :return:
+    """
+
+    i = 0
+    x = x0
+    v = v0
+    lbest = x0
+
+    while i < max_iter:
+        options = criteria(x) > criteria(lbest)
+        lbest = options * x + (1 - options) * lbest
+        gbest = lbest[np.where(np.max(criteria(lbest)) == criteria(lbest))][0]
+
+        x, v = update_xv(x, v, dt, a1, a2, gbest, lbest, omega)
+
+        i += 1
