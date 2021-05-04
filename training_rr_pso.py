567342f5885085688325061d069e972685c35636
RubenIbarrondo <rubenibarrondo@gmail.com>
2021-05-04 15:52:22 +0200

    Add covid_aragon.csv

--- training_rr_pso.py
+++ training_rr_pso.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import rr_pso
 
+from mayavi import mlab
+import moviepy.editor as mpy
+
 
 def griewank(x):
     # [-600, 600]^D
@@ -42,18 +45,59 @@ def mean_error(x, x_opt=None):
 
 
 if __name__ == '__main__':
+    # TEST CON 30 PARÁMETROS
+
+    # me_arr = []
+    # error_iter_arr = []
+    # for run in range(100):
+    #     X, V = training_rr_pso(criteria=lambda y: -griewank(y))
+    #     x = X[-1]
+    #     me_arr.append(mean_error(x))
+    #
+    #     error_iter_arr.append(np.array([mean_error(xi) for xi in X]))
+    #
+    # print(np.median(np.array(me_arr)))
+    #
+    # error_iter = np.mean(np.array(error_iter_arr), axis=0)
+    #
+    # plt.plot(np.log10(error_iter))
+    # plt.show()
+
+    # TEST CON 2 PARÁMETROS
+
     me_arr = []
     error_iter_arr = []
-    for run in range(100):
-        X, V = training_rr_pso(criteria=lambda y: -griewank(y))
-        x = X[-1]
-        me_arr.append(mean_error(x))
 
-        error_iter_arr.append(np.array([mean_error(xi) for xi in X]))
+    X, V = training_rr_pso(criteria=lambda y: -griewank(y), nparams=2)
+    N = X.shape[0]
+    x = X[-1]
+    me_arr.append(mean_error(x))
 
-    print(np.median(np.array(me_arr)))
+    error_iter_arr.append(np.array([mean_error(xi) for xi in X]))
 
-    error_iter = np.mean(np.array(error_iter_arr), axis=0)
+    xp = X[:, :, 0]
+    yp = X[:, :, 1]
+    zp = np.array([griewank(X[g, :, :]) for g in range(N)])
 
-    plt.plot(np.log10(error_iter))
-    plt.show()
+    d = 500
+    Xmesh, Ymesh = np.meshgrid(np.linspace(-d, d, 500), np.linspace(-d, d, 500))
+    Xflat = Xmesh.flatten()
+    Yflat = Ymesh.flatten()
+    Zmesh = griewank(np.column_stack([Xflat, Yflat])).reshape(Xmesh.shape)
+
+    mlab.mesh(Xmesh, Ymesh, Zmesh)
+    sp = mlab.points3d(xp[N-1], yp[N-1], zp[N-1], np.ones(zp[0].shape), scale_factor=10)
+    #mlab.show()
+
+    @mlab.animate(delay=700, support_movie=True)
+    def anim():
+       for i in range(len(xp)):
+           #s.mlab_source.scalars = np.sin((x*x+y*y+i*np.pi)/10)
+
+           sp.mlab_source.y = yp[i]
+           sp.mlab_source.x = xp[i]
+           sp.mlab_source.z = zp[i]
+           yield
+
+    anim()
+    mlab.show()
